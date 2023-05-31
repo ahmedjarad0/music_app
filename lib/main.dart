e6c3d67980a694764e4a1d19c08cecbf84325f6c
oalshokri <janataanfor@gmail.com>
2023-05-31 15:59:41 +0300

    - final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,11 +13,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Expanded _buildKey({required Color color, required int num}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.all(0),
+        ),
+        onPressed: () {
+          play(num);
+        },
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  void play(int num) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$num.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildKey(color: Colors.red, num: 1),
+              _buildKey(color: Colors.orange, num: 2),
+              _buildKey(color: Colors.yellow, num: 3),
+              _buildKey(color: Colors.green, num: 4),
+              _buildKey(color: Colors.blue, num: 5),
+            ],
+          ),
+        ),
       ),
     );
   }
